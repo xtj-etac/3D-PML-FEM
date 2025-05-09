dd99c2b85007e97ca1c4a21398d157c71a2ba5d2
xtj-etac <50580927+xtj-etac@users.noreply.github.com>
2025-05-09 17:23:55 +0800

    Delete myenv.sh

--- myenv.sh
+++
@@ -1,6 +0,0 @@
-
-export PARMETIS_ROOT=/thfs1/home/test653/shutian/YHMAX/parmetis-4.0.3
-export PARMETIS_BUILD_DIRS=${PARMETIS_ROOT}/build/Linux-aarch64
-export LD_LIBRARY_PATH=/thfs1/home/test653/shutian/YHMAX/parmetis-4.0.3/build/_install/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/thfs1/home/test653/lc/lapack-3.10.0/build/lib:$LD_LIBRARY_PATH
-
